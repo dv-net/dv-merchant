a67da06a854f5363cd3e1855ceaef22ed2784a4e
Robert Allen <robert.allen@dv.net>
2025-10-01 12:01:09 +0300

    DV-3277: fixed locale forwarding on external API

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - fix: add error handling for empty withdrawal addresses and improve logging format [DV-3437]
 - Added currency reference data about it being native, and contract address [DV-3456]
 - Added new currencies to the supported currency pool (disabled by default for new stores) [DV-3319]
+- Fixed locale forwarding on external endpoint [DV-3277]
 
 ## [0.9.7] - 2025-09-22
 - Fix rename ResetPasswordCode to Code for consistency in user notifications [DV-3403]
--- internal/delivery/http/request/wallet_request/create.go
+++ internal/delivery/http/request/wallet_request/create.go
@@ -5,7 +5,7 @@ import "github.com/shopspring/decimal"
 type CreateRequest struct {
 	StoreID         string          `json:"store_id,omitempty" validate:"required,uuid" format:"uuid"`
 	StoreExternalID string          `json:"store_external_id" validate:"required" format:"uuid"`
-	Amount          decimal.Decimal `json:"amount,omitempty" query:"amount" validate:"omitempty"`
+	Amount          decimal.Decimal `json:"amount" query:"amount" validate:"omitempty"`
 	Currency        *string         `json:"currency,omitempty" query:"currency" validate:"omitempty"`
 	Email           *string         `json:"email,omitempty" query:"email" validate:"omitempty,email" format:"email"`
 	Locale          *string         `json:"locale,omitempty" query:"locale" validate:"omitempty"`
@@ -15,7 +15,7 @@ type ExternalCreateRequest struct {
 	StoreExternalID string          `json:"store_external_id" query:"store_external_id" validate:"required" format:"uuid"`
 	IP              *string         `json:"ip,omitempty" query:"ip" validate:"omitempty,ip" format:"ipv4"`
 	Email           *string         `json:"email,omitempty" query:"email" validate:"omitempty,email" format:"email"`
-	Amount          decimal.Decimal `json:"amount,omitempty" query:"amount" validate:"omitempty"`
+	Amount          decimal.Decimal `json:"amount" query:"amount" validate:"omitempty"`
 	Currency        *string         `json:"currency,omitempty" query:"currency" validate:"omitempty"`
 	Locale          *string         `json:"locale,omitempty" query:"locale" validate:"omitempty"`
 } // @name CreateWalletExternalRequest
