8fd412566fba2a92c6f70cc47bc5c3db3d6c2e39
Robert Allen <robert.allen@dv.net>
2025-10-15 12:45:26 +0300

    fix: incorrect Binance spot order rules calculation

--- pkg/exchange_client/errors.go
+++ pkg/exchange_client/errors.go
@@ -9,6 +9,8 @@ var (
 	ErrSoftLockByUserSecurityAction    = errors.New("temporary disabled due to user security action") // password reset/change, etc.
 	ErrWithdrawalBalanceLocked         = errors.New("withdrawal balance locked")
 	ErrMinWithdrawalBalance            = errors.New("withdrawal threshold not met")
+	ErrInsufficientBalance             = errors.New("insufficient balance")
+	ErrSymbolTradingHalted             = errors.New("symbol trading is halted")
 	ErrWithdrawalAddressNotWhitelisted = errors.New("withdrawal address not whitelisted")
 	ErrWithdrawalPending               = errors.New("withdrawal pending")
 	ErrInvalidAddress                  = errors.New("invalid address")
