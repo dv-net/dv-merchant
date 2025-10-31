4ee3b3b0e66f021e2cc698cfb35c1a3e50b74a10
Robert Allen <robert.allen@dv.net>
2025-10-31 14:31:15 +0300

    fix: use correct exchange client's shared error

--- internal/service/exchange/errors.go
+++ internal/service/exchange/errors.go
@@ -2,20 +2,8 @@ package exchange
 
 import (
 	"errors"
-	"syscall"
 )
 
 var (
-	ErrExchangeNotFound        = errors.New("exchange not found")
-	ErrUnsupportedExchangeType = errors.New("invalid exchange type")
-	ErrInsufficientBalance     = errors.New("insufficient balance")
-	ErrInvalidIPAddress        = errors.New("invalid IP address")
-	ErrSkipOrder               = errors.New("skip order")
+	ErrExchangeNotFound = errors.New("exchange not found")
 )
-
-var KnownNetworkErrors = []error{
-	syscall.ECONNRESET,
-	syscall.EPIPE,
-	syscall.ECONNREFUSED,
-	syscall.ETIMEDOUT,
-}
